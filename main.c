9516fbb2ae092da9b9d642b974414bc0a0910f82
Amrit <amrit.kaur2@hotmail.com>
2017-09-27 15:52:21 +1300

    works

--- main.c
+++ main.c
@@ -1,43 +1,48 @@
 /*
- * A3a.c
+ * Assignment 3 - MCU.c
  *
- * Created: 21/09/2017 3:37:02 PM
- * Author : ktp5126
+ * Created: 11/09/2017 3:37:02 PM
+ * Author : Alvin Kumar & Amrit Kaur
  */ 
 
-#define F_CPU 8000000UL
 #include <avr/io.h>
-#include <LabBoard.h>
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 #include <avr/interrupt.h>
 
+//TX/RX defines
 #define RX_FULL		(UCSR1A & (1 << RXC1))
 #define TX_EMPTY	(UCSR1A & (1 << UDRE1))
 #define TX_COMPLETE	(UCSR1A & (1 << TXC1))
-#define readPINA	0x01
-#define checkTX     0x00
+#define READ_PINA	0x01
+#define CHECK_TX    0x00
 #define SET_PORTC   0x0A
-#define START_BYTE  0x53
+//Switch State Defines
+#define start		0
+#define instruct	1
+#define msb			2
+#define lsb			3
+#define stop		4
+
+
+char START_BYTE = 0x53;
 #define STOP_BYTE   0xAA
-#define TX_VALID 0x0F
+#define TX_VALID	0x0F
 
-//char led[8] = "00010001";
-//int i = 0;
-//int b = 7;
-//char line0[20];
+
+
+//Data variables for incoming data
 unsigned char data;
-//int x = 0;
+unsigned char msbDATA;
+unsigned char lsbDATA;
+char state;
 
+int flag = 0;
+
+//Function prototypes
 void setup(void);
-void UARTSend(unsigned char data);
 ISR (USART1_RX_vect);
-volatile enum comms State;	// Communications state machine
-volatile uart rxData;
-volatile unsigned char dataStatus;
-
-
 
 int main(void)
 {
@@ -45,122 +50,68 @@ int main(void)
 
     while (1) 
     {
-	if (dataStatus)
-        {
-            dataStatus = 0;
-            memcpy(&data, &rxData, sizeof(rxData));
-            
-            switch(data.INS)
-            {
-                case checkTX:
-                    UARTSendByte(TX_VALID);
-                    break;
-                case read_PINA:
-                    UARTSendByte(PINA);
-                    break;
-                case SET_PORTC:
-                    PORTC = data.LSB;
-                    UARTSendByte(data.INS);
-                    break;
-                    
-                default:
-                    UARTSendByte(0xFF);	// C# program expects something back.
-                    break;
-            }
-        }
-	UARTSend(PINA);
-		
+		if(data == CHECK_TX)				//if instruction data from UART is to check transmission			
+		{	
+			UDR1 = TX_VALID;					//Uart O/P the yes TX was successful byte
+		}
+		else if (data == READ_PINA)				//if instruction data is to read PINA byte 
+		{
+			UDR1 = PINA;						//Uart O/P the PINA data 
+		}
+		else if (flag == 1)			//if instruction data is Set PORTC byte
+		{
+			PORTC = msbDATA;			//O/P the data that is saved in the msb and lsb data variables to PORTC
+			flag = 0;
+		}
     }
 }
 
 void setup(void)
 {
-	DDRA = 0x00;		//set PORTA for input
-	DDRC = 0xFF;		// set PORTC for output
-	DDRE = 0b00000011;	//PORTE is required to be setup for
-	PORTE = 0x00;		//PORTC - PORTA lab board interface.
-    
-    UCSR1A = 0x00;
-	UBRR1L = 12;		//38.4k baud rate
-	UCSR1B = 0b10011000; //RX interrupts , RX and TX enabled
-	UCSR1C = 0b00000110; //async, no parity, 8 data bits, 1 stop bit
-	sei();
-	SLCDInit();         				// Initialize the LCD.
-	SLCDDisplayOn();    				// Switched On the LCD.
-	SLCDClearScreen();				// clear the LCD
+	DDRA 	= 0x00;			//set PORTA for input
+	DDRC	= 0xFF;			// set PORTC for output
+	DDRE 	= 0b00000011;	//PORTE is required to be setup for
+	PORTE 	= 0x00;			//PORTC - PORTA lab board interface.
+	UBRR1L 	= 12;			//38.4k baud rate
+	UCSR1B 	= 0b10011000;	//interrupts off, RX and TX enabled
+	UCSR1C 	= 0b00000110;	//async, no parity, 8 data bits
+	sei();					//interupts enabled.
+	
+
 }
 
 ISR (USART1_RX_vect)
 {
-    char data;
-    data = UDR1;
-    
-    // i'm thinking all this is built in due to the capitals, if not then -_-
-    switch(State)
-    {
-            // Start Byte
-        case startCom:
-            if(data == START_BYTE)
-            {
-                State = InsCom;	// Change state to INS
-            }
-            break;
-            // Instruction Byte
-        case InsCom:
-            if(data >= checkTX)	// Change state to STOP
-            {
-                rxData.INS = data;
-                State = stopCom;
-            }
-            else if(data >= SET_PORTC)	// Change state to LSB
-            {
-                rxData.INS = data;
-                State = lsbCom;
-            }
-            else	// Invalid INS. Back to START
-            {
-                State = startCom;
-            }
-            break;
-            // Data (LSB) Byte
-        case lsbCom:
-            rxData.LSB = data;
-            State = msbCom;
-            break;
-            // Data (MSB) Byte
-        case msbCom:
-            rxData.MSB = data;
-            State = stopCom;
-            break;
-            // Stop Byte
-        case stopCom:
-            // If message correctly terminated, set data ready flag
-            if(data == STOP_BYTE)
-            {
-                dataStatus = 1;	// Flag: Data Ready
-            }
-            // No matter what, go back to START
-            State = startCom;
-            break;
-    }
+	data = UDR1;
+
+	switch(state)
+	{
+		case start:						// Default state: Check if incoming data is required Start Byte
+			if (data == START_BYTE)
+				state = instruct;		//If so, move to instruction state
+		break;
+		case instruct:				//save the incoming byte to data variable
+			if (data >= SET_PORTC)		//if the data is set portc instruction, switch state to MSB
+			{
+				state = msb;
+			}
+									
+			else
+				state = stop;
+		break;
+		case msb:
+			msbDATA = data;				//save the next incoming byte to msbDATA variable, switch state to lsb save the next byte
+			state = lsb;
+		case  lsb:						//save the LSB byte, switch to stop state
+			lsbDATA = data;
+			flag = 1;
+			state = stop;
+		break;
+		case stop:						//check if next byte is stop byte, if so return to start and await next RX
+			if (data == STOP_BYTE)
+				state = start;
+		break;
+		
+	}
 	
 }
-
-void UARTSend(unsigned char data)
-{
-	while (!TX_EMPTY);
-	UDR1 = data;
-	while (!TX_COMPLETE);
-	UCSR1A |= (1<<TXC1);
-}
-
-
-
-// NOTE: structures below goes in UART.h <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
-enum comms {startCom, stopCom, InsCom, msbCom, lsbCom};
-typedef struct
-{
-    unsigned char INS;	// Instruction byte
-    unsigned char MSB;	// uint16_t - MSB
-    unsigned char LSB;	// uint16_t - LSB
-}uart;
